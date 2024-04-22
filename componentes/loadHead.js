46611e86f243d7ecdee944e5af3735d7afc16a0d
Lazaro-Laptop <Lazaro-Laptop@email.com>
2024-04-22 09:45:59 +0100

    Design fixed

--- componentes/loadHead.js
+++ componentes/loadHead.js
@@ -1,4 +1,4 @@
 document.head.innerHTML += `
-	<link rel="shortcut icon" href="./design/isotipo-titulo.png" type="image/png">
+	<link rel="shortcut icon" href="./design/isotipo-titulo.webp" type="image/webp">
 	<link rel="stylesheet" href="/style.css">
 `;
