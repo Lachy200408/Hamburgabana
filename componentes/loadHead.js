fecae6d31a2587cddef72abc05fdef8601fc4ff1
Lazaro-Laptop <Lazaro-Laptop@email.com>
2024-05-05 23:58:09 +0100

    SEO fixed

--- componentes/loadHead.js
+++ componentes/loadHead.js
@@ -1,10 +1,4 @@
 document.head.innerHTML += `
 	<link rel="shortcut icon" href="./design/isotipo-titulo.webp" type="image/webp">
 	<link rel="stylesheet" href="/style.css">
-	<meta name="description" content="La tienda de hamburguesas más grande de Cuba. Debes unirte a nuestra familia, no lo dudes. Disfruta de las delicias de nuestra casa.">
-	<meta name="keywords" content="HABANA,CUBA,HAMBURGUESAS,HAMBURGABANA,HOSTELERIA,PANES,BURGUER">
-	<meta name="author" content="Lázaro Parra González">
-	<meta property="og:image" content="https://github.com/Lachy200408/Hamburgabana/blob/master/design/Hamburguesa_branding.webp" />
-	<meta property="og:description" content="La tienda de hamburguesas más grande de Cuba. Debes unirte a nuestra familia, no lo dudes. Disfruta de las delicias de nuestra casa." />
-	<meta property="og:title" content="Hamburgabana Burger Shop" />
 `;
